ce5928b5ce27488a3585ae0dc41d35cd97bbc78d
T3sultan <incognito.soul011@gmail.com>
2021-03-18 00:30:24 +0600

    Html markup

---
+++ src/http.js
@@ -0,0 +1,34 @@
+class Http{
+    async get(url){
+    const respone =  await fetch(url).then(data=>data.json())
+        return respone;
+
+    }
+    async post(url,data){
+      const response =  await fetch(url,{
+            method:"POST",
+            body:JSON.stringify(data),
+            headers:{
+                'Content-type':'application/json; charset-UTF-8',
+            },
+        }).then(data=>data.json())
+        return response;
+    }
+
+    async put(url,data){
+        const respone = await fetch(url,{
+            method:"PUT",
+            body:JSON.stringify(data),
+            headers:{
+                'Content-type':'application/json; charset-UTF-8',
+            },
+        }).then(data=>data.json())
+        return respone;
+    }
+    async delete(url){
+        const respone = await fetch(url,{
+        method:"DELETE"
+        }).then(data=>data.json())
+        return respone;
+    }
+}
