f7286fd22394bcd9b9bfd1c1d3e21d70cae7675d
T3sultan <incognito.soul011@gmail.com>
2021-03-18 02:28:17 +0600

    UI

--- src/ui.js
+++ src/ui.js
@@ -1,5 +1,35 @@
 export default class UI{
-   paint(){
+    constructor(){
+        this.contactInput = document.getElementById('contacts');
+        this.firstNameInput = document.getElementById('firstName');
+        this.lastNameInput = document.getElementById('lastName');
+        this.emailInput = document.getElementById('email');
+        this.phoneInput = document.getElementById('phone');
+    }
+   paint(contacts){
+       let output ='';
+       contacts.forEach(contact=>{
+           const {firstName,lastName,email,phone}=contact;
+           output +=`
+           <div class="card" class="mb-3">
+           <div class="card-body">
+               <h5 class="card-title ">${firstName} ${lastName}</h5>
+               <p class="card-text ">${email}</p>
+               <p clase="card-text ">${phone}</p>
+               <a href="#" id="edit" class="mr-3 ">
+                   <i class="fas fa-pencil-alt  "></i>
+               </a>
+               <a href="#" id="delete" ">
+                   <i class="fas fa-trash"></i>
+
+               </a>
+
+           </div>
+
+       </div>
+           `
+       });
+         this.contactInput.innerHTML=output;
 
    }
 }
